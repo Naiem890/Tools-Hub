3e4f75985a0c9b386f4525883d6d0f8675182731
Naiem890 <solaiman.naiem890@gmail.com>
2022-05-25 06:19:47 +0600

    firebase connected + social login added

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,35 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logoImage from "./../../../assets/image/logo.svg";
+import SocialLogin from "./SocialLogin";
 const Login = () => {
   return (
     <div className="my-10">
       <div className="max-w-sm mx-auto shadow bg-white p-5 text-center">
         <img src={logoImage} alt="" className="inline-block mt-4" />
         {/* <h2 className="text-3xl font-medium mt-4">Sign In</h2> */}
-        <div className="mt-6">
-          <button
-            type="button"
-            class="btn w-full text-black  bg-sky-100 border-none hover:bg-slate-200"
-          >
-            <svg
-              class="w-4 h-4 mr-2 -ml-1"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="google"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 488 512"
-            >
-              <path
-                fill="currentColor"
-                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-              ></path>
-            </svg>
-            Continue with Google
-          </button>
-        </div>
+        <SocialLogin></SocialLogin>
         <div class="divider">OR</div>
         <form action="" className="flex flex-col gap-2 mt-4">
           <div class="form-control w-full">
---
+++ src/firebase.init.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  /* apiKey: "AIzaSyCXF-dnTkegerKWIE1uRL3WcFve-k6tTDI",
+  authDomain: "tools-hub-e018a.firebaseapp.com",
+  projectId: "tools-hub-e018a",
+  storageBucket: "tools-hub-e018a.appspot.com",
+  messagingSenderId: "779779962927",
+  appId: "1:779779962927:web:9cd3dfe4907f913ca32eda", */
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+
+console.log(firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+export default auth;
