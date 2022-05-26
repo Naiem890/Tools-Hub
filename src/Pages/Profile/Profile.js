4001c9ae73dc21922940e0664c26936e78e66bc9
Naiem890 <solaiman.naiem890@gmail.com>
2022-05-27 04:19:15 +0600

    image uploading bug fixed

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,7 +1,8 @@
 import { PhotographIcon } from "@heroicons/react/outline";
 import axios from "axios";
+import { updateProfile } from "firebase/auth";
 import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { useAuthState, useUpdateProfile } from "react-firebase-hooks/auth";
 import { useForm } from "react-hook-form";
 import auth from "../../firebase.init";
 import Loading from "../Shared/Loading";
@@ -9,39 +10,38 @@ import Loading from "../Shared/Loading";
 const Profile = () => {
   const [user, loading, error] = useAuthState(auth);
   const { register, handleSubmit, reset } = useForm();
+  const [updateProfile, updating, error1] = useUpdateProfile(auth);
 
-  if (loading) {
+  if (loading || updating) {
     return <Loading></Loading>;
   }
 
-  if (error) {
-    return "error ocurred: " + error.message;
+  if (error || error1) {
+    return "error ocurred: " + error?.message;
   }
 
   const handleUpdate = async (updatedInfo) => {
-    console.log(updatedInfo);
-
     const profileImage = updatedInfo.profileImage[0];
     console.log(profileImage);
-    const formData = new FormData();
-    formData.append("profileImage", profileImage);
 
-    const api_key = "4f64d6de9449c916e1d70aba9cdc8689";
-    await fetch(`https://api.imgbb.com/1/upload?key=${api_key}`, {
-      method: "POST",
-      body: formData,
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
+    let body = new FormData();
+    body.set("key", "4f64d6de9449c916e1d70aba9cdc8689");
+    body.append("image", profileImage);
 
-    /* await axios
-      .post(
-        `https://api.imgbb.com/1/upload?key=2e98b583f59c2b8eb6abdd2106835d68`,
-        formData
-      )
-      .then((res) => console.log(res.data))
-      .catch((error) => console.log(error)); */
+    axios({
+      method: "post",
+      url: "https://api.imgbb.com/1/upload",
+      data: body,
+    }).then(async (res) => {
+      if (res.data.status == 200) {
+        await updateProfile({
+          displayName: updatedInfo.displayName,
+          photoURL: res.data.data.url,
+        });
+        // console.log(res.data);
+      }
+      return console.log(res.data.data.url);
+    });
   };
 
   return (
@@ -55,15 +55,15 @@ const Profile = () => {
                 <img src={user.photoURL} alt="userPhoto" />
               </div>
             </div>
-            <input type="file" {...register("profileImage")} />
-            {/* <div class="flex justify-center items-center w-full pr-4">
+            {/* <input type="file" {...register("profileImage")} /> */}
+            <div class="flex justify-center items-center w-full pr-4">
               <label
                 for="dropzone-file"
-                class="flex flex-col justify-center items-center w-full  bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                class="flex flex-col justify-center items-center w-full  bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer  hover:bg-gray-100 "
               >
                 <div class="flex flex-col justify-center items-center my-4">
                   <PhotographIcon className="h-8"></PhotographIcon>
-                  <p class="text-sm text-gray-500 dark:text-gray-400">
+                  <p class="text-sm text-gray-500">
                     <span class="font-semibold">Click to upload photo</span>
                   </p>
                 </div>
@@ -71,10 +71,10 @@ const Profile = () => {
                   id="dropzone-file"
                   type="file"
                   {...register("profileImage")}
-                  class=""
+                  class="hidden"
                 />
               </label>
-            </div> */}
+            </div>
           </div>
 
           <div className="form-control w-full mt-2">
